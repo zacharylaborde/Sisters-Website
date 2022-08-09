4afcdda8ef0bd18b3e4693ef1ccdc216446bca22
Zachary Laborde <ZacharyLaborde@gmail.com>
2022-08-09 15:51:54 -0400

    replaced lorem ipsum with an about me.

--- forms.py
+++ forms.py
@@ -1,3 +1,4 @@
+""" Sample module docstring """
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
