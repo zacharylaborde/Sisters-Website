1338785398f89e8970a63dca71aac5bb3144104f
Zachary Laborde <ZacharyLaborde@gmail.com>
2022-08-09 14:26:40 -0400

    added basic pages

--- application.py
+++ application.py
@@ -5,9 +5,28 @@ application = Flask(__name__)
 
 
 @application.route('/')
-def index():
+@application.route('/home')
+def home():
     """ Sample function docstring """
-    return render_template('index.html')
+    return render_template("home.html")
+
+
+@application.route('/services')
+def services():
+    """ Sample function docstring """
+    return "<h1>Services</h1>"
+
+
+@application.route('/contact')
+def contact():
+    """ Sample function docstring """
+    return "<h1>Contact Me</h1>"
+
+
+@application.route('/faq')
+def faq():
+    """ Sample function docstring """
+    return "<h1>FAQ</h1>"
 
 
 if __name__ == '__main__':
